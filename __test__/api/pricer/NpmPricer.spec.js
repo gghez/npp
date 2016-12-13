4b9828a97d5497497313b8cd8a5372b498f1f3cc
gghez <gregory.ghez@gmail.com>
2016-12-13 08:20:05 +0100

    format

--- __test__/api/pricer/NpmPricer.spec.js
+++ __test__/api/pricer/NpmPricer.spec.js
@@ -1,20 +1,29 @@
 import { NpmPricer } from "../../../src/api/pricer/NpmPricer";
 
+const testPkg = {
+    modified: '2016-12-09T20:13:55.558Z',
+    author: {
+        name: 'main3',
+        email: 'main1mail'
+    },
+    maintainers: [{ name: 'main1', email: 'main1mail' }, { name: 'main2', email: 'main2mail' }],
+    downloads: 7000
+};
+
 test('getPricingData() returns correct values', () => {
     const pricer = new NpmPricer();
-    const pricingData = pricer.getPricingData({
-        modified: '2016-12-09T20:13:55.558Z',
-        author: {
-            name: 'main3',
-            email: 'main1mail'
-        },
-        maintainers: [{ name: 'main1', email: 'main1mail' }, { name: 'main2', email: 'main2mail' }],
-        downloads: 7000
-    });
+    const pricingData = pricer.getPricingData(testPkg);
 
     expect(pricingData).toEqual({
         lastActivity: '2016-12-09T20:13:55.558Z',
         people: 2,
         downloads: 7000
     });
+});
+
+test('price() returns correct values', () => {
+    const pricer = new NpmPricer();
+    const price = pricer.price(testPkg);
+
+    expect(price).toEqual()
 });
