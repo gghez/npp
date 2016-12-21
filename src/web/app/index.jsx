3f6be29f52277c4a807bbdcd8929248e7de46cb1
gghez <gregory.ghez@gmail.com>
2016-12-21 08:58:31 +0100

    router

--- src/web/app/index.jsx
+++ src/web/app/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router, Route, browserHistory, IndexRedirect } from "react-router";
+import { Router, Route, browserHistory, IndexRedirect, IndexRoute } from "react-router";
 import { App } from "./app.jsx";
+import { Home } from "./home.jsx"; ƒ
 import { PackageList } from "./packageList.jsx"
 
 render(
     <Router history={browserHistory}>
         <Route path="/" component={App}>
+            <IndexRoute component={Home} />
             <Route path="search">
                 <IndexRedirect to="/" />
                 <Route path=":search" component={PackageList} />
